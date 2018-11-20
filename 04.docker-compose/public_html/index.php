11e0f06518e412791ffef08bd5089638e809bb48
Pavel Matviienko <shturman.p@gmail.com>
2018-11-20 13:41:42 +0200

    - added docker-compose example

--- 04.docker-compose/public_html/index.php
+++ 04.docker-compose/public_html/index.php
@@ -1,15 +1,27 @@
 <h1>Hello From Docker-compose</h1>
-<pre>
-<?php
-echo "MYSQL:\n";
-try{
-    $connection = new PDO("mysql:host=db-server", 'root', 'root');
-} catch( \Exception $e){
-    echo $e->getMessage();
-}
-var_dump($connection->query('SHOW DATABASES;')->fetchAll());
-echo "--------------------------------------------------------\n";
-echo "SERVER:\n";
-var_dump($_SERVER);
-?>
-</pre>
+<table>
+    <tr>
+        <th>Mysql</th>
+        <th>$_SERVER</th>
+    </tr>
+    <tr>
+        <td valign="top">
+            <pre>
+                <?php
+                try{
+                    $connection = new PDO("mysql:host=db-server", 'root', 'root');
+                } catch( \Exception $e){
+                    echo $e->getMessage();
+                }
+                var_dump($connection->query('SHOW DATABASES;')->fetchAll());
+                ?>
+            </pre>
+        </td>
+        <td>
+            <pre>
+                <?php var_dump($_SERVER); ?>
+            </pre>
+
+        </td>
+    </tr>
+</table>
